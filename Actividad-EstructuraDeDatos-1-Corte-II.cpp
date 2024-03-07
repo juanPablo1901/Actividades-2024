d27acdb4ae8879cc65d7be168060f56124e5d737
juanPablo1901 <158856564+juanPablo1901@users.noreply.github.com>
2024-03-06 21:06:26 -0500

    Update Actividad-EstructuraDeDatos-1-Corte-II.cpp

--- Actividad-EstructuraDeDatos-1-Corte-II.cpp
+++ Actividad-EstructuraDeDatos-1-Corte-II.cpp
@@ -2,140 +2,98 @@
 #include <conio.h>
 using namespace std;
 
-void llenar_tabla(int arreglo[], int n);
-void imprimir_tabla(int arreglo[], int n);
-void Burbuja(int arreglo[], int n);
-/*void Binario(int arreglo[], int n);
-void Seleccion(int arreglo[],int n);*/
-
 
 
 int main()
 {
-int n;
-int temp; 
-int opcion;
-int M;   
-int arreglo[n];
-int tabla[M];
+	int n;
+	int temp;   
+	int arreglo[n];
+	int i, p, pos, aux, max, min, mitad;
+		char band = 'F';
+ 	
+		min = 0;
+		max = n;
+	
 
-	cout<<"Tamano de la tabla: "<<endl;
+cout<<"Tamano de la tabla: "<<endl;
     cin>>n;
     	if(n<0){
     	cout<<"No se puede poner numeros negativos: "<<endl;
 		}
-
-do {
-        cout << "MENU:" << endl;
-        cout << "1. Llenar tabla " << endl;
-        cout << "2. Ver tabla " << endl;
-        cout << "3. Ordenar por metodo burbuja" << endl;
-       /* cout << "4. Ordenar por metodo Binario" << endl;
-        cout << "5. Ordenamiento por seleccion" << endl;*/
-		cout << "0. Salir" << endl;
-        cout << "Seleccione una opcion: ";
-        cin >> opcion;
-
-        switch (opcion) {
-            case 1:
-                cout << "Ingrese un numero: ";
-                llenar_tabla(tabla,M);
-                break;
-            case 2:
-                imprimir_tabla(tabla,M);
-                break;
-            case 3:
-            	Burbuja(tabla,M);
-            	break;
-            case 0:
-                cout << "Saliendo del programa." << endl;
-                break;
-            default:
-                cout << "Opcion invalida. Por favor, seleccione una opcion valida." << endl;
-                break;
-        }
-    } while (opcion != 0);
-	
-    getch();
-    return 0;
-}
-
-void llenar_tabla(int arreglo[], int n){
-	
-	for (int i=0; i<n; i++) {
+		
+		for (int i=0; i<n; i++) {
     cout <<"Numero "<<i+1<<" : "<<endl;
     cin >> arreglo[i];
 	}
-}
-
-void imprimir_tabla(int arreglo[], int n){
-	cout<<"Salida: "<<endl;
-    for(int i=0; i<n; i++){
-    	cout<<"[ "<<arreglo[i]<<" ]";
-	}
-}
-
-void Burbuja(int arreglo[], int n){
-	
-	    /*ordenar*/
-    for (int i=0; i<n; i++){
-        /*comparación*/
+			
+	for (int i=0; i<n; i++){
+        /*comparaciÃ³n*/
         for(int j=i+1; j<n; j++){
         /*intercambiar datos*/
         if (arreglo[i]>arreglo[j]){
             int temp = arreglo[j]; 
             arreglo[j]=arreglo[i];
             arreglo[i]=temp;
-            
-            }
-        }
-    } 
-	
-}
+        	}
+    	}
+	}
 
-/*
-(){
-	
-	int arreglo3[] = {6,9,7,8,10};
-	int i, pos, aux;
-	
-	for(i=0; i<5; i++){
+	for(i=0; i<n; i++){
 	
 	pos = i;
-	aux = arreglo3[i];
+	aux = arreglo[i];
 	
-	while((pos>0) && (arreglo3[pos-1] > aux)){
-		arreglo3[pos] = arreglo3[pos-1];
+	while((pos>0) && (arreglo[pos-1] > aux)){
+		arreglo[pos] = arreglo[pos-1];
 		pos--;
 	
 		}
 	
-	arreglo3[pos] = aux;
+	arreglo[pos] = aux;
+	}
+
+	cout<<"Salida Burbuja: "<<endl;
+    for(int i=0; i<n;i++){
+    	cout<<"[ "<<arreglo[i]<<" ]";
 	}
 	
-	cout<<"Salida: "<<endl;
-    for(int i=0; i<5;i++){
-    	cout<<"[ "<<arreglo3[i]<<" ]";
+		cout<<"\n Salida Ordenamineto por insercion: "<<endl;
+    for(int i=0; i<n;i++){
+    	cout<<"[ "<<arreglo[i]<<" ]";
 	}
 	
-}
-
-*/
-
-
-/* Binario
-
-int min, max, mitad,v,p,m;
-int arr[];
-min arr = arr [m];
-max arr = arr[p];
-mitad = arr[(max-min)/2];
-
-cin>>v;
-while(v!=mitad){
-    if(v>mitad){
+	
+		cout<<"\n ingrese el numero que desea buscar" <<endl;
+	cin>>p;
+	
+	while(min <= max){
+    	
+		mitad=(min+max)/2;
+		
+		if(arreglo[mitad] == p){
+			band = 'V';
+			break;
+		}
+		
+		if(arreglo[mitad] > p){
+			max = mitad;
+			mitad = (min+max)/2;
         
-    }
+    	}
+    	
+    	if(arreglo[mitad] < p){
+    		min = mitad;
+    		mitad = (min+max)/2;
+		}
 }
-*/
+
+if(band == 'V'){
+	cout<<"El numero a sido encontrado en la posicion: "<<mitad<<endl;
+	}
+else {
+	cout<<"No se encontro el dato"<<endl;
+	}
+	
+}
 
