7efb0d1babd8c93f2f508e160f3b0221405f5d6e
juanPablo1901 <158856564+juanPablo1901@users.noreply.github.com>
2024-03-14 00:56:45 -0500

    Add files via upload

--- Actividad-2024-3.cpp
+++ Actividad-2024-3.cpp
@@ -2,90 +2,162 @@
 #include <conio.h>
 using namespace std;
 
-
-struct nodo
-{
+struct nodo {
     int dato;
     nodo* siguiente;
 };
 
-void insertarLista(nodo *&,int);
+void insertarLista(nodo *&, int);
 void mostrarLista(nodo *);
+void eliminarNodo(nodo *&, int);
+void calcularMayorMenor(nodo *);
+void sumar(nodo *);
 
+int main() {
+    nodo *list = NULL;
+    int opcion;
+    int m;
+    int verificar;
+    int valorAEliminar;
 
+    do {
+    cout<<"\n|-------------------------------------------------------|";
+	cout<<"\n|               *LISTA CIRCULAR SIMPLE*                 |";
+	cout<<"\n|-------------------------------------------------------|";
+	cout<<"\n|   1. Insertar un numero  |   4. Ver mayor y menor     |";
+	cout<<"\n|   2. Mostrar la lista    |   5. Sumar todo            |";
+	cout<<"\n|   3. Eliminar un numero  |   0. Salir del sistema     |";
+	cout<<"\n|-------------------------------------------------------|";
+	cout<<"\n Escoja una Opcion: ";
+        cin >> opcion;
 
-int main(){
-	nodo *list = NULL;
-	int m;
-	int verificar;
-	
-	cout<<"Ingrese un numero: "<<"["<<"] \n"<<endl;
-	do{
-		
-		cin>>m;
-		insertarLista(list, m);
-		cout<<"Desea continuar?"<<"["<<"1 = si"<<"]"<<"["<<"0 = no"<<"] \n"<<endl;
-		cin>>verificar;
-		
-		if(verificar>=2){
-			verificar = 0;
-		}
-		}
-		if(verificar<=-1){
-			verificar = 0;
-		}
-		if(verificar==false){
-			cout<<"No se pueden ingresar letras al sistema \n"<<endl;
-		}
-		
-		cout<<"Ingresa un numero"<<"["<<"] \n"<<endl;
-		
-		
-	} while(verificar!=0);
-	
-	mostrarLista(list);
-	
-	
-	getch();
-	return 0;
+        switch (opcion) {
+            case 1:
+                cout << "Ingrese un numero: ";
+                cin >> m;
+                insertarLista(list, m);
+                break;
+            case 2:
+                mostrarLista(list);
+                break;
+            case 3:
+                cout << "Ingrese el valor que desea eliminar: ";
+                cin >> valorAEliminar;
+                eliminarNodo(list, valorAEliminar);
+                break;
+            case 4:
+            	cout <<"\n Mostrar mayor y menor \n" << endl;
+                calcularMayorMenor(list);
+                break;
+            case 5:
+            	cout <<"\n SUMA \n" << endl;
+                sumar(list);
+                break;
+            case 0:
+                cout << "Saliendo del programa." << endl;
+                break;
+            default:
+                cout << "Opcion invalida. Por favor, seleccione una opcion valida." << endl;
+                break;
+        }
+    } while (opcion != 0);
+
+    getch();
+    return 0;
+}
+
+void insertarLista(nodo *&list, int n) {
+    nodo *nuevo_nodo = new nodo();
+    nuevo_nodo->dato = n;
+
+    nodo *aux1 = list;
+    nodo *aux2;
+
+    while ((aux1 != NULL) && (aux1->dato < n)) {
+        aux2 = aux1;
+        aux1 = aux1->siguiente;
+    }
+    if (list == aux1) {
+        list = nuevo_nodo;
+    } else {
+        aux2->siguiente = nuevo_nodo;
+    }
+
+    nuevo_nodo->siguiente = aux1;
+
+    cout << "Elemento " << n << " insertado en la lista" << endl;
+}
+
+void mostrarLista(nodo *list) {
+    nodo *actual = list;
+
+    cout << "Lista: ";
+    while (actual != NULL) {
+        cout << "[" << actual->dato << "] -> ";
+        actual = actual->siguiente;
+    }
+    cout << "NULL" << endl;
+}
+
+void eliminarNodo(nodo *&list, int valor) {
+    if (list != NULL) {
+        nodo *aux_borrar;
+        nodo *anterior = NULL;
+        aux_borrar = list;
+
+        while (aux_borrar != NULL) {
+            if (aux_borrar->dato == valor) {
+                // Si el nodo a eliminar es el primero
+                if (anterior == NULL) {
+                    list = list->siguiente;
+                    delete aux_borrar;
+                    aux_borrar = list;
+                } else {
+                    anterior->siguiente = aux_borrar->siguiente;
+                    delete aux_borrar;
+                    aux_borrar = anterior->siguiente;
+                }
+            } else {
+                anterior = aux_borrar;
+                aux_borrar = aux_borrar->siguiente;
+            }
+        }
+
+        cout << "\n Se han eliminado todos los nodos con el valor " << valor << endl;
+    } else {
+        cout << "\n La lista está vacía." << endl;
+    }
 }
 
 
-void insertarLista(nodo *&list,int n){
-	nodo *nuevo_nodo = new nodo();
-	nuevo_nodo->dato = n;
+void calcularMayorMenor(nodo *lista){
+	int mayor = 0, menor=9999;
 	
-	nodo *aux1 = list;
-	nodo *aux2;
-	
-	while((aux1 != NULL) && (aux1->dato<n)){
-		aux2= aux1;
-		aux1 = aux1->siguiente;
-	}
-	if(list == aux1){
-		list = nuevo_nodo;
-	}
-	else {
-		aux2->siguiente = nuevo_nodo;
+	while(lista!=NULL){
+		if((lista->dato) > mayor){
+			mayor = lista->dato;
+		}
+		if((lista->dato)<menor){
+			menor = lista->dato;
+		}
+		lista = lista->siguiente;
 	}
 	
-	nuevo_nodo->siguiente = aux1;
-	
-	cout<<"Elemento \n"<<n<<"\ninsertado en la lista \n"<<endl;
-	
+	cout<<"\n El mayor es: "<<mayor<<endl;
+	cout<<"\n El menor es: "<<menor<<endl;
 }
 
-void mostrarLista(nodo *list){
-	nodo *actual = new nodo();
-	actual = list;
+void sumar(nodo *lista){
+	int suma;
+	int prom=0;
 	
-	while(actual !=NULL){
-		cout<<"["<<actual->dato<<"] -> ";
-		actual = actual->siguiente;
+	while(lista!=NULL){
+		suma=lista->dato+lista->dato;
+		prom++;
+		lista = lista->siguiente;
 	}
+	cout<<"\n La suma es: "<<suma<<endl;
+	cout<<"\n Su promedio es: "<<(suma+0.0)/prom<<endl;
 	
 }
-
-
-
 
---
+++ struct c++.cpp
@@ -0,0 +1,237 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct nodo {
+    int dato;
+    nodo* siguiente;
+}*primero, *ultimo;
+
+void insertarNodo();
+void BuscarNodo();
+void ModificarNodo();
+void EliminarNodo();
+void mostrarNodo();
+void calcularMayorMenor();
+/* nodo * */
+
+
+
+int main (){
+	int opcion=0;
+	
+	do{
+	cout<<"\n|---------------------------------------------|";
+	cout<<"\n|     *LISTA CIRCULAR SIMPLE*                 |";
+	cout<<"\n|---------------------------------------------|";
+	cout<<"\n|   1.Insertar   |   4.Eliminar   |  7.salir  |";
+	cout<<"\n|   2.Buscar     |   5.Mostrar    |           |";
+	cout<<"\n|   3.Modificar  |   6.Mayor&Meor |           |";
+	cout<<"\n|---------------------------------------------|";
+	cout<<"\n Escoja una Opcion: ";
+	cin>>opcion;
+	switch (opcion) {
+            case 1:
+                cout << "\n Insertar en un Nodo \n";
+                insertarNodo();
+                break;
+            case 2:
+            	cout<<"\n Buscar un Nodo \n";
+                BuscarNodo();
+                break;
+            case 3:
+                cout << "\n Modificar un Nodo \n";
+                ModificarNodo();
+                break;
+            case 4:
+                cout << "\n Eliminar Nodo \n" << endl;
+                EliminarNodo();
+                break;
+            case 5:
+                cout << "\n Mostrar Struct \n" << endl;
+                mostrarNodo();
+                break;
+            case 6:
+                cout << "\n Mostrar mayor y menor \n" << endl;
+                calcularMayorMenor();
+                break;
+            case 7:
+                cout << "Saliendo del programa." << endl;
+                break;
+            default:
+                cout << "Opcion invalida. Por favor, seleccione una opcion valida." << endl;
+                break;
+        }
+	
+}while(opcion!=7);
+	
+	return 0;	
+}
+
+
+void insertarNodo(){
+	nodo *nuevo_nodo = new nodo();
+	cout<<"\n Dato: "<<endl;
+    cin>>nuevo_nodo->dato;
+   	if( primero==NULL){
+   		primero=nuevo_nodo;
+   		primero->siguiente=primero;
+   		ultimo = primero;
+	   } else{
+	   	ultimo->siguiente = nuevo_nodo;
+	   	nuevo_nodo->siguiente = primero;
+	   	ultimo = nuevo_nodo;
+	   	
+	   }
+	   cout<<"\n Nodo creado"<<endl;
+}
+	   
+
+void BuscarNodo(){
+	nodo *actual = new nodo;
+    actual = primero;
+	bool encontrado = false;
+	int nodoBuscado = 0;
+	cout<<"\n Nodo a Buscar: \n"<<endl;
+	cin>>nodoBuscado;
+	if(primero!=NULL){
+		
+		do{
+			if(actual->dato==nodoBuscado){
+				cout<< "El nodo con el dato < "<<nodoBuscado<<" > Encontrado \n"<<endl;
+				encontrado = true;
+			}
+			actual = actual->siguiente;
+	
+		}while (actual != primero && encontrado != true);
+		if (!encontrado){
+			cout<<"\n Nodo no Encontrado \n"<<endl;
+		}
+	}else{
+		cout<<"\n La lista esta vacia"<<endl;
+	}
+	
+}
+
+void ModificarNodo(){
+	nodo* actual = (nodo*)malloc(sizeof(nodo));
+	actual = primero;
+	int nodoBuscado=0, encontrado=0;
+	cout<<"\n Ingrese el valor del nodo a buscar para modificarlo: ";
+	cin>>nodoBuscado;
+	if(primero!=NULL){
+		do{
+			if(actual->dato == nodoBuscado){
+				cout<<"\n Nodo con el dato "<<nodoBuscado<<" \n encontrado";
+				cout<<"\n Ingrese el nuevo dato para este Nodo: ";
+				scanf ("%d", &actual->dato);
+				cout<<"\n Nodo modificado con exito \n";
+				encontrado = 1;
+			}
+			actual = actual->siguiente;
+		}while(actual!=primero && encontrado!=1);
+		
+		if(encontrado==0){
+			cout<<"\n Nodo no encontrado ";
+		}
+		
+	}else{
+		cout<<"\n La lista se encuentra vacia \n";
+	}
+	
+}
+
+void EliminarNodo(){
+	nodo *actual = (nodo*)malloc(sizeof(nodo));
+    actual = primero;
+    nodo* anterior = (nodo*)malloc(sizeof(nodo));
+    anterior = NULL;
+	int nodoBuscado = 0, encontrado = 0;
+	cout<<"\n Ingrese el valor de nodo a buscar para Eliminar: \n"<<endl;
+	cin>>nodoBuscado;
+	if(primero!=NULL){
+		do{
+			if(actual->dato==nodoBuscado){
+				cout<< "El nodo con el dato < "<<nodoBuscado<<" > Encontrado \n"<<endl;
+				if(actual==primero){
+					primero = primero->siguiente;
+					ultimo->siguiente=primero;
+				}else if(actual==ultimo){
+					anterior->siguiente=primero;
+					ultimo=anterior;
+				}else{
+					anterior->siguiente=actual->siguiente;
+				}
+				cout<<"\n Nodo Eliminado \n";
+				encontrado = 1;
+			}
+			anterior=actual;
+			actual=actual->siguiente;
+	
+		}while (actual != primero && encontrado != 1);
+		if (encontrado==0){
+			cout<<"\n Nodo no Encontrado \n"<<endl;
+		}else{
+			free(anterior);
+		}
+	}else{
+		cout<<"\n La lista esta vacia"<<endl;
+	}
+	
+}
+
+void mostrarNodo() {
+    nodo *actual = new nodo;
+    actual = primero;
+	if(primero!=NULL){
+		
+		do{
+		cout << " [" << actual->dato << "] ";
+        actual = actual->siguiente;
+        
+		}while (actual != primero); 
+		
+	}else{
+		cout<<"\n La lista esta vacia"<<endl;
+	}
+}
+
+void calcularMayorMenor(){
+	int mayor = 0, menor=9999;
+	nodo *actual = new nodo;
+    actual = primero;
+	while(primero!=NULL){
+		if((actual->dato) > mayor){
+			mayor = actual->dato;
+		}
+		if((actual->dato)<menor){
+			menor = actual->dato;
+		}
+		actual = actual->siguiente;
+	}
+	
+	cout<<"\n El mayor es: "<<mayor<<endl;
+	cout<<"\n El menor es: "<<menor<<endl;
+}
+
+/*
+void OrdenarPorBurbuja();{
+
+	nodo*actual, *primero, *ultimo;
+	if(nodo!=NULL){
+		actual = nodo;
+		do{
+		
+		primero=actual->siguiente;
+		while(siguiente!=nodo){
+			if(actual->)
+		}
+		
+		
+			
+		}
+	}
+	*/
