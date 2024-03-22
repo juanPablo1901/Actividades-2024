b971206ab7e09003d9177c27c1fa1363d2f950d9
juanPablo1901 <158856564+juanPablo1901@users.noreply.github.com>
2024-03-21 19:24:29 -0500

    Add files via upload

---
+++ cola-1-2024.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+
+struct Nodo {
+	int elemento;
+	Nodo *siguiente; 
+};
+
+void insertarCola(Nodo *&,Nodo *&, int);
+bool cola_vacia(Nodo *);
+void suprimirCola(Nodo *&,Nodo *&, int &);
+
+int main (){
+	
+	Nodo *frente = NULL;
+	Nodo *fin = NULL;
+	int dato, opcion;
+	
+			do{
+	cout<<"\n|-------------------------- |";
+	cout<<"\n|           *COLA*          |";
+	cout<<"\n|---------------------------|";
+	cout<<"\n|         1.Insertar        |";
+	cout<<"\n|         2.Sacar           |";
+	cout<<"\n|         0.Salir           |";
+	cout<<"\n|---------------------------|";
+	cout<<"\n Escoja una Opcion: ";
+	cin>>opcion;
+	switch (opcion) {
+            case 1:
+                cout << "\n Insertar una Pila: ";
+                cin>>dato;
+				insertarCola(frente,fin,dato);
+                break;
+            case 2:
+            	cout<<"\n Quitar: ";
+            	if(frente==NULL){
+					cout<<"\n Error en saca("<<dato<<"). La pila esta vacia \n"<<endl;
+					}	
+                while(frente!=NULL){
+                	suprimirCola(frente, fin, dato);
+                	if(frente!=NULL){
+                		cout<<" ["<<dato<<"] -";
+					}else{
+						cout<<" ["<<dato<<"] .";
+					}
+				}
+                break;
+            case 0:
+                cout << "Saliendo del programa." << endl;
+                break;
+            default:
+                cout << "Opcion invalida. Por favor, seleccione una opcion valida." << endl;
+                break;
+            }
+	}while(opcion!=0);
+	
+	
+	
+	getch();
+	return 0;
+}
+
+
+void insertarCola(Nodo *&frente, Nodo*&fin, int n){
+	
+	Nodo *nuevo_nodo = new Nodo();
+	
+	nuevo_nodo->elemento=n;
+	nuevo_nodo->siguiente=NULL;
+	
+	if (cola_vacia(frente)){
+		frente = nuevo_nodo;
+	}else{
+		fin->siguiente = nuevo_nodo;
+	}
+	
+	fin=nuevo_nodo;
+	
+	cout<<"\n Elemento ["<<n<<"] insertado en la cola correctamente"<<endl;
+	
+}
+
+bool cola_vacia(Nodo *frente){
+	
+	
+	if(frente == NULL){
+		return true;
+	}else{
+		return false;
+	}
+	
+	/*return(frente==NULL)? true : false;*/
+}
+
+
+void suprimirCola(Nodo *&frente, Nodo *&fin, int &n){
+	
+	n = frente->elemento;
+	Nodo *aux = frente;
+	
+	if(frente == fin){
+		frente = NULL;
+		fin = NULL;
+	}else{
+		frente = frente->siguiente;
+	}
+	
+	delete aux;
+}
+
+
+
---
+++ pila-1-2024.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+
+struct Nodo {
+	int elemento;
+	Nodo *siguiente; 
+};
+
+void agregarpila(Nodo *&, int);
+void sacarPila(Nodo *&, int &);
+void eliminarPila(Nodo *&, int &);
+
+int main (){
+	
+	Nodo *pila = NULL;
+	int dato, opcion;
+	
+		do{
+	cout<<"\n|-------------------------- |";
+	cout<<"\n|           *PILA*          |";
+	cout<<"\n|---------------------------|";
+	cout<<"\n|         1.Insertar        |";
+	cout<<"\n|         2.Sacar           |";
+	cout<<"\n|         0.Salir           |";
+	cout<<"\n|---------------------------|";
+	cout<<"\n Escoja una Opcion: ";
+	cin>>opcion;
+	switch (opcion) {
+            case 1:
+                cout << "\n Insertar una Pila: ";
+                cin>>dato;
+				agregarpila(pila,dato);
+                break;
+            case 2:
+            	cout<<"\n Sacar Pila: ";
+            	if(pila==NULL){
+					cout<<"\n Error en saca("<<dato<<"). La pila esta vacia \n"<<endl;
+					}	
+                while(pila!=NULL){
+                	sacarPila(pila,dato);
+                	if(pila!=NULL){
+                		cout<<" ["<<dato<<"] -";
+					}else{
+						cout<<" ["<<dato<<"] .";
+					}
+				}
+                break;
+            case 0:
+                cout << "Saliendo del programa." << endl;
+                break;
+            default:
+                cout << "Opcion invalida. Por favor, seleccione una opcion valida." << endl;
+                break;
+            }
+	}while(opcion!=0);
+	
+	getch();
+	return 0;	
+}
+
+
+void agregarpila(Nodo *&pila, int n){
+	Nodo *nuevo_nodo = new Nodo();
+	nuevo_nodo->elemento = n;
+	nuevo_nodo->siguiente = pila;
+	pila = nuevo_nodo;
+	
+	cout<<"\n Elemento "<<n<<" agregado a Pila correctamente "<<endl;
+}
+
+
+void sacarPila(Nodo *&pila, int &n){	
+	Nodo *aux = pila;
+	n = aux->elemento;
+	pila = aux->siguiente;
+	delete aux;
+}
+	
+	
+
+
+
+
+
+
+
+
+
+
