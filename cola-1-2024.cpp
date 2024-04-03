6ffbf6c9508f9598cf5fe59c7d91f2af8ed3fa67
juanPablo1901 <158856564+juanPablo1901@users.noreply.github.com>
2024-04-02 21:10:01 -0500

    Add files via upload

--- cola-1-2024.cpp
+++ cola-1-2024.cpp
@@ -5,18 +5,24 @@ using namespace std;
 
 struct Nodo {
 	int elemento;
+	int prioridad;
 	Nodo *siguiente; 
 };
 
 void insertarCola(Nodo *&,Nodo *&, int);
 bool cola_vacia(Nodo *);
 void suprimirCola(Nodo *&,Nodo *&, int &);
+void mostrarCola(Nodo *&, Nodo *&, int &);
+void OrdenarPrioridad(Nodo *&);
+
 
 int main (){
 	
+	system("color f1");
+	
 	Nodo *frente = NULL;
 	Nodo *fin = NULL;
-	int dato, opcion;
+	int dato, opcion, pr;
 	
 			do{
 	cout<<"\n|-------------------------- |";
@@ -24,20 +30,21 @@ int main (){
 	cout<<"\n|---------------------------|";
 	cout<<"\n|         1.Insertar        |";
 	cout<<"\n|         2.Sacar           |";
+	cout<<"\n|         3.Mostrar         |";
 	cout<<"\n|         0.Salir           |";
 	cout<<"\n|---------------------------|";
 	cout<<"\n Escoja una Opcion: ";
 	cin>>opcion;
 	switch (opcion) {
             case 1:
-                cout << "\n Insertar una Cola: ";
+                cout << "\n Insertar una Pila: ";
                 cin>>dato;
 				insertarCola(frente,fin,dato);
                 break;
             case 2:
             	cout<<"\n Quitar: ";
             	if(frente==NULL){
-					cout<<"\n Error en sacar("<<dato<<"). La }Cola esta vacia \n"<<endl;
+					cout<<"\n Error en saca("<<dato<<"). La pila esta vacia \n"<<endl;
 					}	
                 while(frente!=NULL){
                 	suprimirCola(frente, fin, dato);
@@ -47,6 +54,10 @@ int main (){
 						cout<<" ["<<dato<<"] .";
 					}
 				}
+                break;
+            case 3:
+                cout << "\n Mostrando: ";
+				mostrarCola(frente,fin,dato);
                 break;
             case 0:
                 cout << "Saliendo del programa." << endl;
@@ -83,8 +94,8 @@ void insertarCola(Nodo *&frente, Nodo*&fin, int n){
 	
 }
 
+
 bool cola_vacia(Nodo *frente){
-	
 	
 	if(frente == NULL){
 		return true;
@@ -111,5 +122,45 @@ void suprimirCola(Nodo *&frente, Nodo *&fin, int &n){
 	delete aux;
 }
 
+void mostrarCola(Nodo *&frente, Nodo *&fin, int &n){
+	
+	struct Nodo *nuevo_nodo;
+	nuevo_nodo = frente->siguiente;
+	cout<<"Caracter   Prioridad"<<endl;
+	cout<<"---------------------"<<endl;
+	
+	while(nuevo_nodo!=NULL){
+		cout<<" "<<nuevo_nodo->elemento<<" | "<<nuevo_nodo->prioridad<<endl;
+		nuevo_nodo=nuevo_nodo->siguiente;
+	}
+}
+
+void OrdenarPrioridad(Nodo *&frente){
+	
+	struct Nodo *aux1, *aux2;
+	int pAux;
+	char cAux;
+	
+	aux1 = frente; 
+	while(aux1->siguiente !=NULL){
+		
+		aux2 = aux1->siguiente;
+			
+		while(aux2 !=NULL){
+			if (aux1->prioridad > aux2->prioridad){
+				pAux = aux1->prioridad;
+				cAux = aux1->elemento;
+				
+				aux1->prioridad = aux2->prioridad;
+				aux1->elemento = aux2->elemento;
+				
+				aux2->prioridad = pAux;
+				aux1->elemento = cAux;
+			}	
+			aux2 = aux2->siguiente;
+		}	
+		aux1 = aux1->siguiente;
+	}
+}
 
 
