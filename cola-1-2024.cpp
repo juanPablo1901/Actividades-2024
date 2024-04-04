fc37ba71d54d0dda0da67b1bfcfe2e6770011a59
juanPablo1901 <158856564+juanPablo1901@users.noreply.github.com>
2024-04-03 20:01:20 -0500

    Add files via upload

--- cola-1-2024.cpp
+++ cola-1-2024.cpp
@@ -29,22 +29,26 @@ int main (){
 	cout<<"\n|           *COLA*          |";
 	cout<<"\n|---------------------------|";
 	cout<<"\n|         1.Insertar        |";
-	cout<<"\n|         2.Sacar           |";
-	cout<<"\n|         3.Mostrar         |";
+	cout<<"\n|         2.Mostrar         |";
+	cout<<"\n|         3.Sacar           |";
 	cout<<"\n|         0.Salir           |";
 	cout<<"\n|---------------------------|";
 	cout<<"\n Escoja una Opcion: ";
 	cin>>opcion;
 	switch (opcion) {
             case 1:
-                cout << "\n Insertar una Pila: ";
+                cout << "\n Insertar una Cola: ";
                 cin>>dato;
 				insertarCola(frente,fin,dato);
-                break;
+                break;    
             case 2:
+                cout << "\n Mostrando: \n";
+				mostrarCola(frente,fin,dato);
+                break;
+            case 3:
             	cout<<"\n Quitar: ";
             	if(frente==NULL){
-					cout<<"\n Error en saca("<<dato<<"). La pila esta vacia \n"<<endl;
+					cout<<"\n Error en saca("<<dato<<"). La cola esta vacia \n"<<endl;
 					}	
                 while(frente!=NULL){
                 	suprimirCola(frente, fin, dato);
@@ -54,10 +58,6 @@ int main (){
 						cout<<" ["<<dato<<"] .";
 					}
 				}
-                break;
-            case 3:
-                cout << "\n Mostrando: ";
-				mostrarCola(frente,fin,dato);
                 break;
             case 0:
                 cout << "Saliendo del programa." << endl;
@@ -124,43 +124,11 @@ void suprimirCola(Nodo *&frente, Nodo *&fin, int &n){
 
 void mostrarCola(Nodo *&frente, Nodo *&fin, int &n){
 	
-	struct Nodo *nuevo_nodo;
-	nuevo_nodo = frente->siguiente;
-	cout<<"Caracter   Prioridad"<<endl;
-	cout<<"---------------------"<<endl;
-	
-	while(nuevo_nodo!=NULL){
-		cout<<" "<<nuevo_nodo->elemento<<" | "<<nuevo_nodo->prioridad<<endl;
-		nuevo_nodo=nuevo_nodo->siguiente;
+	while(frente!=NULL){
+		cout<<frente->elemento<<"\n";
+		frente=frente->siguiente;
 	}
 }
 
-void OrdenarPrioridad(Nodo *&frente){
-	
-	struct Nodo *aux1, *aux2;
-	int pAux;
-	char cAux;
-	
-	aux1 = frente; 
-	while(aux1->siguiente !=NULL){
-		
-		aux2 = aux1->siguiente;
-			
-		while(aux2 !=NULL){
-			if (aux1->prioridad > aux2->prioridad){
-				pAux = aux1->prioridad;
-				cAux = aux1->elemento;
-				
-				aux1->prioridad = aux2->prioridad;
-				aux1->elemento = aux2->elemento;
-				
-				aux2->prioridad = pAux;
-				aux1->elemento = cAux;
-			}	
-			aux2 = aux2->siguiente;
-		}	
-		aux1 = aux1->siguiente;
-	}
-}
 
 
